4ec967c37278732fd5d777a10e4a58fb200a3300
Tuisku <tuiskuwood@outlook.com>
2019-09-18 01:46:53 +0800

    fix: 修复 bukkitVersion 下的物品相关功能.

--- src/tech/v2c/minecraft/plugins/jsonApi/RESTful/actions/ItemAction.java
+++ src/tech/v2c/minecraft/plugins/jsonApi/RESTful/actions/ItemAction.java
@@ -1,77 +1,80 @@
 package tech.v2c.minecraft.plugins.jsonApi.RESTful.actions;
 
-import org.bukkit.entity.Item;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import tech.v2c.minecraft.plugins.jsonApi.RESTful.global.BaseAction;
 import tech.v2c.minecraft.plugins.jsonApi.RESTful.global.annotations.ApiRoute;
 import tech.v2c.minecraft.plugins.jsonApi.RESTful.global.entities.JsonData;
 import tech.v2c.minecraft.plugins.jsonApi.RESTful.global.entities.item.ItemDTO;
-import tech.v2c.minecraft.plugins.jsonApi.tools.results.JsonResult;
 import tech.v2c.minecraft.plugins.jsonApi.tools.gameUtils.UserUtils;
+import tech.v2c.minecraft.plugins.jsonApi.tools.results.JsonResult;
 
 import java.util.ArrayList;
 
 public class ItemAction extends BaseAction {
-//    @ApiRoute(Path = "/api/Item/GetList")
-//    public JsonResult GetItemList() {
-//        ArrayList<ItemDTO> list = new ArrayList<ItemDTO>();
-//        for (int i = 0; i < Item.list.length; i++) {
-//            Item item = Item.get(i);
-//            if(item.getName().toLowerCase().contains("unknown")){
-//                continue;
-//            }
-//            ItemDTO itemDTO = new ItemDTO();
-//            itemDTO.setId(item.getId());
-//            itemDTO.setName(item.getName());
-//            itemDTO.setHasMeta(item.hasMeta());
-//            list.add(itemDTO);
-//        }
-//
-//        return new JsonResult(list);
-//    }
-//
-//    @ApiRoute(Path = "/api/Item/GetItemInfo")
-//    public JsonResult GetItemInfo(JsonData data){
-//        int itemId = (int) Double.parseDouble(data.Data.get("item").toString());
-//
-//        Item item = Item.get(itemId);
-//
-//        ItemDTO itemDTO = new ItemDTO();
-//        itemDTO.setId(item.getId());
-//        itemDTO.setName(item.getName());
-//        itemDTO.setHasMeta(item.hasMeta());
-//
-//        return new JsonResult(itemDTO);
-//    }
-//
-//    @ApiRoute(Path = "/api/Item/SendItemToUser")
-//    public JsonResult SendItem(JsonData data) {
-//        String userName = data.Data.get("name").toString();
-//        int itemId = (int) Double.parseDouble(data.Data.get("item").toString());
-//        Object count = data.Data.get("count");
-//        Object meta = data.Data.get("meta");
-//        Object msg = data.Data.get("message");
-//
-//        Player player = UserUtils.GetPlayerByName(userName);
-//        if (player == null) return new JsonResult(null, 404, "Error: user not found.");
-//
-//        if (player.getInventory().getSize() > 0) {
-//            return new JsonResult(null, 409, "Error: Player's Inventory is full.");
-//        } else {
-//            int firstEmpty = player.getInventory().firstEmpty(null);
-//            Item item = Item.get(itemId, meta == null ? 100 : (int) Double.parseDouble(meta.toString()), count == null ? 1 : (int) Double.parseDouble(count.toString()));
-//            if(item.getName().toLowerCase().contains("unknown")){
-//                return new JsonResult(null, 404, "Error: item not found.");
-//            }
-//            boolean result = player.getInventory().setItem(firstEmpty, item);
-//            if(player.getInventory().setItem(firstEmpty, item)){
-//                if(msg != null){
-//                    player.sendMessage(msg.toString());
-//                }
-//                return new JsonResult(result);
-//            }else{
-//                return new JsonResult(null, 400, "Error: can not send item to player.");
-//            }
-//        }
-//    }
+    @ApiRoute(Path = "/api/Item/GetList")
+    public JsonResult GetItemList() {
+        ArrayList<ItemDTO> list = new ArrayList<ItemDTO>();
+
+        Material[] items = Material.values();
+
+        for (int i = 0; i < items.length; i++) {
+            ItemDTO itemDTO = new ItemDTO();
+            itemDTO.setId(items[i].getId());
+            itemDTO.setName(items[i].name());
+            list.add(itemDTO);
+        }
+
+        return new JsonResult(list);
+    }
+
+    @ApiRoute(Path = "/api/Item/GetItemInfo")
+    public JsonResult GetItemInfo(JsonData data) {
+        int itemId = (int) Double.parseDouble(data.Data.get("item").toString());
+
+        Material item = Material.getMaterial(itemId);
+        if(item == null){
+            return new JsonResult(null, 404, "Error: item not found.");
+        }
+        ItemDTO itemDTO = new ItemDTO();
+        itemDTO.setId(item.getId());
+        itemDTO.setName(item.name());
+
+        return new JsonResult(itemDTO);
+    }
+
+    @ApiRoute(Path = "/api/Item/SendItemToUser")
+    public JsonResult SendItem(JsonData data) {
+        String userName = data.Data.get("name").toString();
+        int itemId = (int) Double.parseDouble(data.Data.get("item").toString());
+        Object count = data.Data.get("count");
+        Object msg = data.Data.get("message");
+
+        Player player = UserUtils.GetPlayerByName(userName);
+        if (player == null) return new JsonResult(null, 404, "Error: user not found.");
+
+        if (player.getInventory().firstEmpty() == -1) {
+            return new JsonResult(null, 409, "Error: Player's Inventory is full.");
+        } else {
+            int firstEmpty = player.getInventory().firstEmpty();
+
+            Material mate = Material.getMaterial(itemId);
+            if (mate == null) {
+                return new JsonResult(null, 404, "Error: item not found.");
+            }
+
+            ItemStack item = new ItemStack(mate, (int) Double.parseDouble(count.toString()));
+
+            player.getInventory().setItem(firstEmpty, item);
+            player.updateInventory();
+
+            if (msg != null) {
+                player.sendMessage(msg.toString());
+                return new JsonResult();
+            } else {
+                return new JsonResult(null, 400, "Error: can not send item to player.");
+            }
+        }
+    }
 }
